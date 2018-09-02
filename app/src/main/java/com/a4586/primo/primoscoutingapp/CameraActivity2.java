9d6a4600dfeeb04807a5df405e7a7f1a711b8869
Itamar <itamarkn60@gmail.com>
2018-09-02 13:39:02 +0300

    Some Prototyping

--- app/src/main/java/com/a4586/primo/primoscoutingapp/CameraActivity2.java
+++ app/src/main/java/com/a4586/primo/primoscoutingapp/CameraActivity2.java
@@ -1,38 +1,61 @@
 package com.a4586.primo.primoscoutingapp;
 
+import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Environment;
 import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageException;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 import com.squareup.picasso.Picasso;
 
 import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class CameraActivity2 extends AppCompatActivity {
 
     static final int REQUEST_CODE_PICTURE = 101;
     ImageView viewImage;
+    Button btnCamera;
+    StorageReference Storage;
+    ProgressDialog Progress;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_camera2);
+        Storage = FirebaseStorage.getInstance().getReference();
 
-        Button btnCamera = (Button) findViewById(R.id.btnCamera);
+        btnCamera = (Button) findViewById(R.id.btnCamera);
         viewImage = (ImageView)findViewById(R.id.imageView);
 
 
+        Progress = new ProgressDialog(this);
 
         btnCamera.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
                 startActivityForResult(intent,REQUEST_CODE_PICTURE);
             }
         });
@@ -41,10 +64,13 @@ public class CameraActivity2 extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-            ByteArrayOutputStream bos = new ByteArrayOutputStream();
-            Bitmap bitmap = (Bitmap) data.getExtras().get("data");
-            bitmap.compress(Bitmap.CompressFormat.JPEG, 50,bos);
-            viewImage.setImageBitmap(bitmap);
+        if (requestCode == REQUEST_CODE_PICTURE && resultCode == Activity.RESULT_OK) {
+            Bitmap photo = (Bitmap) data.getExtras().get("data");
+            viewImage.setImageBitmap(photo);
+        }
+           }
 
     }
-}
+
+
+
